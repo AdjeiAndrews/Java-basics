dee7e46cf4b5898be19c7c3541479dcb8624e076
Adjei Andrews <andrewsadjei777@gmail.com>
2025-11-23 13:10:48 +0000

    using for-loop to create 9 tiles

--- src/main/java/org/example/WhacAMole.java
+++ src/main/java/org/example/WhacAMole.java
@@ -5,7 +5,7 @@ import java.awt.event.*;
 import java.util.Random;
 
 
-public class WhacAMole {
+public class WhacAMole{
 
     int boardWidth = 600;
     int boardHeight = 650;
@@ -13,6 +13,9 @@ public class WhacAMole {
     JFrame frame = new JFrame("Mario: Whack A Mole");
     JLabel textLabel = new JLabel();
     JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
+
+    JButton[] board = new JButton[9];
 
     public WhacAMole() {
 
@@ -23,7 +26,7 @@ public class WhacAMole {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
-        textLabel.setFont(new Font("Arial", Font.BOLD, 50));
+        textLabel.setFont(new Font("Arial", Font.PLAIN, 50));
         textLabel.setHorizontalAlignment(JLabel.CENTER);
         textLabel.setText("Score: 0");
         textLabel.setOpaque(true);
@@ -32,9 +35,14 @@ public class WhacAMole {
         textPanel.add(textLabel);
         frame.add(textPanel, BorderLayout.NORTH);
 
-        JPanel boardPanel = new JPanel();
         boardPanel.setLayout(new GridLayout(3, 3));
-        boardPanel.setBackground(Color.black);
+        //boardPanel.setBackground(Color.black);
         frame.add(boardPanel);
+
+        for (int i = 0; i < 9; i++) {
+            JButton tile  = new JButton();
+            board[i] = tile;
+            boardPanel.add(tile);
+        }
     }
 }
